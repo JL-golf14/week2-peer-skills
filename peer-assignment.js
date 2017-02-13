dc95637a55c8fdcd61c11ba80c73b1ad0d87c5f8
Linh Kieu Le <linhkieule@Linhs-MacBook-Pro-2.local>
2017-02-13 17:20:28 -0600

    hard Mode

--- peer-assignment.js
+++ peer-assignment.js
@@ -1,16 +1,34 @@
+$(document).ready(function(){
 
 
-    var arrayP = [{name: 'Tony', yearsOfExperience: 10}, {name: 'Carla', yearsOfExperience: 4}];
+
+
+  arrayP.forEach(function(input){
+    $('#yearsIn').append(
+      '<tr>'+'<td>'+ input.name + '</td>' +'<td>'+ "years of experience  " + input.yearsOfExperience + '</td>' +
+      '</tr>'
+
+
+    );
+
+  });
+  $('h4').append("Total Years of all employees  " + totalYears);
+
+});
+
+
+
+var arrayP = [{name: 'Tony', yearsOfExperience: 10}, {name: 'Carla', yearsOfExperience: 4}];
 var totalYears = 0;
 
-var taco =function(arrayP){
+var totalYearsCalculator =function(arrayP){
 
 
-    arrayP.forEach(function(input){
-      totalYears += input.yearsOfExperience;
+  arrayP.forEach(function(input){
+    totalYears += input.yearsOfExperience;
 
-    });
+  });
 
-return totalYears;
+  return totalYears;
 }
-console.log(taco(arrayP));
+console.log(totalYearsCalculator(arrayP));
