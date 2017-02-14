41290daf8773b78ed3193e1b95b0913abee876bb
Linh Kieu Le <linhkieule@Linhs-MacBook-Pro-2.local>
2017-02-14 10:51:27 -0600

    fixing problems

--- peer-assignment.js
+++ peer-assignment.js
@@ -2,33 +2,29 @@ $(document).ready(function(){
 
 
 
+  $('form').on('submit',function(event){
+    event.preventDefault();
+    var submissionArray = $(this).serializeArray();
+    var newEmployeeObject = {};
 
-  arrayP.forEach(function(input){
-    $('#yearsIn').append(
-      '<tr>'+'<td>'+ input.name + '</td>' +'<td>'+ "years of experience  " + input.yearsOfExperience + '</td>' +
-      '</tr>'
 
+    submissionArray.forEach(function(inputFieldObject){
+      newEmployeeObject[inputFieldObject.name] = inputFieldObject.value;
 
-    );
+    });
+arrayP.push(newEmployeeObject);
+    var totalYears = 0;
+    arrayP.forEach(function(person){
+      $('#yearsIn').append(
+        '<tr>'+'<td>'+ person.name+ '</td>' +'<td>'+ "years of experience  " + person.yearsOfExperience + '</td>' +
+        '</tr>'
+      );
+      totalYears += person.yearsOfExperience;
+    });
+    $('h4').append("Total Years of all employees  " + totalYears);
 
   });
-  $('h4').append("Total Years of all employees  " + totalYears);
+
+  var arrayP = [{name: 'Tony', yearsOfExperience: 10}, {name: 'Carla', yearsOfExperience: 4}];
 
 });
-
-
-
-var arrayP = [{name: 'Tony', yearsOfExperience: 10}, {name: 'Carla', yearsOfExperience: 4}];
-var totalYears = 0;
-
-var totalYearsCalculator =function(arrayP){
-
-
-  arrayP.forEach(function(input){
-    totalYears += input.yearsOfExperience;
-
-  });
-
-  return totalYears;
-}
-console.log(totalYearsCalculator(arrayP));
